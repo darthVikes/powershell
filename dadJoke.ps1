8dc53c98a5669aabc81ca8be75872f7800fadc93
James Wuerflein <james.wuerflein@InfiniteCampus.com>
2020-04-01 21:19:10 -0500

    add new next joke function

--- dadJoke.ps1
+++ dadJoke.ps1
@@ -1,10 +1,23 @@
 Function Get-DadJoke {
      Invoke-WebRequest -Uri "https://icanhazdadjoke.com" -Headers @{accept="application/json"} | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Joke
 }
+
+Function Get-NextJoke {
+     $joke = Get-DadJoke
+     Write-host "joke: " $joke
+     $speak.Speak($joke)
+}
+
+[parameter(Mandatory = $false)]
+[int]$numberOfJokes=1
+
 Add-Type -AssemblyName System.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 
-$joke = Get-DadJoke
-Write-host "joke: " $joke
-$speak.Speak($joke)
+$count=0
+do {
+     Get-NextJoke
+     $count++
+} while ($count -lt $numberOfJokes)
+
 
